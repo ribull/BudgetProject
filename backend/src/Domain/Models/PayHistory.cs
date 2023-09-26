d27b642098dfa9eec3ab35a56b259edb3b2c31f0
ribull <ribull44@gmail.com>
2023-09-25 20:04:10 -0400

    Added Pay History and tests (#2)

---
+++ backend/src/Domain/Models/PayHistory.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Models;
+
+#nullable disable
+
+public record PayHistory
+{
+    public int PayHistoryId { get; init; }
+
+    public DateTime PayPeriodStartDate { get; init; }
+
+    public DateTime PayPeriodEndDate { get; init; }
+
+    public double Earnings { get; init; }
+
+    public double PreTaxDeductions { get; init; }
+
+    public double Taxes { get; init; }
+
+    public double PostTaxDeductions { get; init; }
+
+    public double NetPay { get => Earnings - PreTaxDeductions - Taxes - PostTaxDeductions; }
+
+    public bool IsAdditionalCompensation { get => NetPay == 0.0; }
+}
