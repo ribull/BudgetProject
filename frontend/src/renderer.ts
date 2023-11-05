731f963cd3f31df04c893cbc3fc2a16e428032c2
ribull <ribull44@gmail.com>
2023-11-05 10:17:31 -0500

    Navigation setup

--- frontend/src/renderer.ts
+++ frontend/src/renderer.ts
@@ -29,3 +29,38 @@
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+
+function openNav(event: Event) {
+  let targetElement: Element = event.currentTarget as Element;
+  let tab: string = targetElement.id.split('-')[1];
+
+  // Display the tab content
+  let tabContents: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.nav-tab-content');
+  for (let i = 0; i < tabContents.length; i++)
+  {
+    if (tabContents[i].id === `nav-content-${tab}`) {
+      tabContents[i].style.display = 'block';
+    }
+    else {
+      tabContents[i].style.display = 'none';
+    }
+  }
+
+  // Change the tabs themselves
+  let tabs: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.navbar-item');
+  for (let i = 0; i < tabs.length; i++)
+  {
+    if (tabs[i].id == targetElement.id) {
+      tabs[i].className += ' is-active';
+    }
+    else {
+      tabs[i].className = tabs[i].className.replaceAll(' is-active', '');
+    }
+  }
+}
+
+let navTabs: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.navbar-item');
+for (let i = 0; i < navTabs.length; i++)
+{
+  navTabs[i].addEventListener('click', openNav);
+}
